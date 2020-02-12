c2c5f3480b78b5244b6c20c676b35a2b92980e2f
Omar <omargeerman@gmail.com>
2020-02-12 11:43:25 +0100

    migrated to ts, added new bg image

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+const image = require("../images/Geerman-Web-Solutions-v2.jpg")
+
+import "./reset.css"
+
+const IndexPage = () => <Container></Container>
+
+export default IndexPage
+
+const Container = styled.div`
+  background: url(${image}) center center no-repeat;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+`
