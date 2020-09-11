f9cb4ce67a36ffdcd792f64e10dec51001e8455a
Omar <omargeerman@gmail.com>
2020-09-11 20:25:25 +0200

    gatsby background image added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,45 @@
 import React from "react"
 import styled from "styled-components"
-const image = require("../images/Geerman-Web-Solutions-v2.jpg")
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import BackgroundImage from "gatsby-background-image"
 
 import "./reset.css"
 
-const IndexPage = () => <Container></Container>
+// @ts-ignore
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "gws.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
+  const imageData = data.desktop.childImageSharp.fluid
+  return (
+    <StyledBackgroundImage
+      Tag="section"
+      fluid={imageData}
+      backgroundColor={`#040e18`}
+    >
+      <Container />
+    </StyledBackgroundImage>
+  )
+}
 
 export default IndexPage
 
 const Container = styled.div`
-  background: url(${image}) center center no-repeat;
-  background-size: cover;
   width: 100vw;
   height: 100vh;
+`
+const StyledBackgroundImage = styled(BackgroundImage)`
+  background-position: bottom center;
+  background-repeat: repeat-y;
+  background-size: cover;
 `
